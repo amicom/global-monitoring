2246caf8c779ac57047d1d2de4e07edfa10cceb4
Maor <amicom.pro@gmail.com>
2018-08-01 19:48:48 +0300

    Fixing layout issues with FlexBox in IE (not fully completed)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {MatButtonModule, MatDividerModule, MatMenuModule} from '@angular/materia
 import {MenuComponent} from './menu/menu.component';
 import {MatIconModule} from '@angular/material/icon';
 import {ServersEnvironmentComponent} from './servers-environment/servers-environment.component';
+import {ServerComponent} from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    ServersEnvironmentComponent
+    ServersEnvironmentComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule, MatButtonModule, MatDividerModule, MatMenuModule, MatIconModule
